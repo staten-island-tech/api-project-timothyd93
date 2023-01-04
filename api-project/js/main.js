2e83fd8d54fb59746fda221f96a06a6b99debcd0
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-01-04 13:59:18 -0500

    m

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const URL = "https://api.quotable.io/random";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.content;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData(URL);
