51dc9540ddcac6e8c44a0827cbb13c4b4b306595
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-01-11 14:00:54 -0500

    n

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -2,5 +2,7 @@ const DOMSelectors = {
   response: document.getElementById("api-response"),
   word: document.getElementById("api-word"),
   definition: document.getElementById("api-definition"),
+  searchbar: document.getElementById("wordenter"),
+  searchbtn: document.getElementById("search"),
 };
 export { DOMSelectors };
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,17 +1,27 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
-const word = `bliss`;
-const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-async function getData(URL) {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
-    DOMSelectors.word.textContent = data[0].word;
-    DOMSelectors.definition.textContent =
-      data[0].meanings[0].definitions[0].definition;
-  } catch (error) {
-    console.log(error);
+
+function findWord() {
+  const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${DOMSelectors.searchbar.value}`;
+  async function getData(URL) {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      console.log(data);
+      DOMSelectors.word.textContent = data[0].word;
+      DOMSelectors.definition.textContent =
+        data[0].meanings[0].definitions[0].definition;
+    } catch (error) {
+      DOMSelectors.word.textContent = `ERROR`;
+      DOMSelectors.definition.textContent = ``;
+    }
   }
+  getData(URL);
 }
-getData(URL);
+function clearInputs() {
+  DOMSelectors.searchbar.value = "";
+}
+DOMSelectors.searchbtn.addEventListener("click", function () {
+  findWord();
+  clearInputs();
+});
