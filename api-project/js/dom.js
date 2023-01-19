3f63b05e2c325f20623132c06f82e53223a46b90
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-01-19 13:59:10 -0500

    a

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,7 +1,9 @@
 const DOMSelectors = {
   name: document.getElementById("pokemonname"),
   abilitylist: document.getElementById("pokemonabilities"),
-  searchbar: document.getElementById("pokemonenter"),
+  searchbar: document.getElementById("searchbar"),
   searchbtn: document.getElementById("search"),
+  header: document.getElementById("header"),
+  content: document.getElementById("content"),
 };
 export { DOMSelectors };
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,91 +1,50 @@
-// import "../styles/style.css";
-// import { DOMSelectors } from "./dom";
+import { DOMSelectors } from "./dom";
+import "../styles/style.css";
 
-// function findWord() {
-//   const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${DOMSelectors.searchbar.value}`;
+function injectPokemon() {
+  const URL = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151/`;
+  async function getData(URL) {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      // DOMSelectors.name.textContent = data.name;
+      data.results.forEach((pokemon) => {
+        let name = pokemon.name;
+        DOMSelectors.content.insertAdjacentHTML(
+          "beforeend",
+          `<div class="pokecard"> <p> Pokemon: ${name} </p> </div>`
+        );
+      });
+    } catch (error) {
+      DOMSelectors.name.textContent = `ERROR`;
+    }
+  }
+  getData(URL);
+}
+
+injectPokemon();
+
+// function makePokemonCard() {
+//   const URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.searchbar.value}`;
 //   async function getData(URL) {
 //     try {
 //       const response = await fetch(URL);
 //       const data = await response.json();
-//       console.log(data);
-//       DOMSelectors.word.textContent = data[0].word;
-//       DOMSelectors.definition.textContent =
-//         data[0].meanings[0].definitions[0].definition;
+//       // DOMSelectors.name.textContent = data.name;
+//       data.abilities.forEach(
+//         (ability) =>
+//           (DOMSelectors.abilitylist.textContent = ability.ability.name)
+//       );
 //     } catch (error) {
-//       DOMSelectors.word.textContent = `ERROR`;
-//       DOMSelectors.definition.textContent = ``;
+//       DOMSelectors.response.textContent = `ERROR`;
 //     }
 //   }
 //   getData(URL);
 // }
 // function clearInputs() {
-//   DOMSelectors.searchbar.value = "";
+//   DOMSelectors.searchbar.value = ``;
 // }
 // DOMSelectors.searchbtn.addEventListener("click", function () {
-//   findWord();
+//   makePokemonCard();
 //   clearInputs();
 // });
-// import { DOMSelectors } from "./dom";
-// const URL = `https://pokeapi.co/api/v2/pokemon/pikachu`;
-// async function getData(URL) {
-//   try {
-//     const response = await fetch(URL);
-//     const data = await response.json();
-//     console.log(data);
-//     console.log(data.name);
-//     data.abilities.forEach((ability) => console.log(ability.ability.name));
-//     data.stats.forEach((stat) => console.log(stat.base_stat));
-//     // DOMSelectors.response.textContent = data.name;
-//   } catch (error) {
-//     DOMSelectors.response.textContent = `ERROR`;
-//   }
-// }
-// getData(URL);
-
-import { DOMSelectors } from "./dom";
-function makePokemonCard() {
-  // const URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.searchbar.value}`;
-  // async function getData(URL) {
-  //   try {
-  //     const response = await fetch(URL);
-  //     const data = await response.json();
-  //     DOMSelectors.name.textContent = data.name;
-  //     // DOMSelectors.abilitylist.textContent = data.abilities.forEach(
-  //     //   (ability) =>
-  //     //     (DOMSelectors.abilitylist.textContent = ability.ability.name)
-  //     //   // (ability) => console.log(ability.ability.name)
-  //     // );
-  //     data.abilities.forEach(
-  //       (ability) =>
-  //         (DOMSelectors.abilitylist.textContent = ability.ability.name)
-  //       // (ability) => console.log(ability.ability.name)
-  //     );
-  //   } catch (error) {
-  //     DOMSelectors.response.textContent = `ERROR`;
-  //   }
-  // }
-  // getData(URL);
-  const URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.searchbar.value}`;
-  async function getData(URL) {
-    try {
-      const response = await fetch(URL);
-      const data = await response.json();
-      DOMSelectors.name.textContent = data.name;
-
-      data.abilities.forEach(
-        (ability) =>
-          (DOMSelectors.abilitylist.textContent = ability.ability.name)
-      );
-    } catch (error) {
-      DOMSelectors.response.textContent = `ERROR`;
-    }
-  }
-  getData(URL);
-}
-function clearInputs() {
-  DOMSelectors.searchbar.value = ``;
-}
-DOMSelectors.searchbtn.addEventListener("click", function () {
-  makePokemonCard();
-  clearInputs();
-});
