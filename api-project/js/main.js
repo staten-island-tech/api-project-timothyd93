462bef6d245e90582adf787494421ca38adf4cc4
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-01-20 13:58:51 -0500

    h

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -8,6 +8,7 @@ function injectPokemon() {
       const response = await fetch(URL);
       const data = await response.json();
       // DOMSelectors.name.textContent = data.name;
+      console.log(data);
       data.results.forEach((pokemon) => {
         let name = pokemon.name;
         DOMSelectors.content.insertAdjacentHTML(
@@ -22,8 +23,38 @@ function injectPokemon() {
   getData(URL);
 }
 
-injectPokemon();
+// injectPokemon();
 
+function clearInputs() {
+  DOMSelectors.searchbar.value = ``;
+}
+
+function searchPokemon() {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.searchbar.value}`;
+  async function getData(URL) {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      // DOMSelectors.name.textContent = data.name;
+      console.log(data);
+      let name = data.name;
+      let img = data.sprites.front_default;
+      DOMSelectors.content.insertAdjacentHTML(
+        "beforeend",
+        `<div class = pokecard> <p> ${name} </p> <img src= ${img}>  </div>`
+      );
+    } catch (error) {
+      // DOMSelectors.name.textContent = `ERROR`;
+      console.log(`Error`);
+    }
+  }
+  getData(URL);
+}
+
+DOMSelectors.searchbtn.addEventListener("click", function () {
+  searchPokemon();
+  clearInputs();
+});
 // function makePokemonCard() {
 //   const URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.searchbar.value}`;
 //   async function getData(URL) {
