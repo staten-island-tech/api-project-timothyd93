ca3b0fc877c1a01741d191d5f08ea1d329d1299d
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2023-01-30 12:11:29 -0500

    b

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,34 +1,27 @@
 import { DOMSelectors } from "./dom";
 import "../styles/style.css";
 
-function injectPokemon() {
-  const URL = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9/`;
+function gen1Pokemon() {
+  const URL = `https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151/`;
   async function getData(URL) {
     try {
       const response = await fetch(URL);
       const data = await response.json();
       console.log(data);
-      data.results.forEach((pokemon) => {
-        let name = pokemon.name;
-        let img = pokemon.sprites.front_default;
-        let ability1 = pokemon.abilities[0].ability.name;
-        let ability2 = pokemon.abilities[1].ability.name;
-        DOMSelectors.content.insertAdjacentHTML(
-          "beforeend",
-          `<div class = "pokecard"> <p id="pokemonname" class ="pokemoncontent"> ${name} </p> <img src= ${img} class ="pokemonimg">  <p  class ="pokemonabilities">${ability1}  </p> <p class="pokemonabilities"> ${ability2}</p>  </div>`
-        );
-      });
+      function pokemonArray() {
+        data.results.forEach((pokemon) => {
+          getData(pokemon.url);
+        });
+      }
     } catch (error) {
       console.log(`Error`);
     }
   }
   getData(URL);
 }
-
 function clearInputs() {
   DOMSelectors.searchbar.value = ``;
 }
-
 function searchPokemon() {
   const URL = `https://pokeapi.co/api/v2/pokemon/${DOMSelectors.searchbar.value}`;
   async function getData(URL) {
@@ -36,10 +29,6 @@ function searchPokemon() {
       const response = await fetch(URL);
       const data = await response.json();
       console.log(data);
-      // console.log(data.abilities[0].ability.name);
-      // console.log(
-      //   data.abilities.forEach((ability) => console.log(ability.ability.name))
-      // );
       let name = data.name;
       let img = data.sprites.front_default;
       let ability1 = data.abilities[0].ability.name;
@@ -62,9 +51,10 @@ function searchPokemon() {
 }
 
 DOMSelectors.searchbtn.addEventListener("click", function () {
-  removeContent();
-  searchPokemon();
-  clearInputs();
+  // removeContent();
+  // searchPokemon();
+  // clearInputs();
+  gen1Pokemon();
 });
 
 function removeContent() {
